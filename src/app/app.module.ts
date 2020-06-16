99e9942a5812f577ce3abf1c2784fbd2ee58a5ff
sahar <SaharElsayed.n@gmail.com>
2020-06-17 00:50:35 +0200

    update product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductLstComponent } from './products/proudectList.component';
+import { ProductLstComponent } from './products/productList.component';
 
 @NgModule({
   declarations: [
--- src/app/products/proudectList.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pm-products',
-    templateUrl: './proudectList.component.html'
-  })
-export class ProductLstComponent{
-// test 
-}
