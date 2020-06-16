b32a453f079974236870d000404703a754eea189
sahar <SaharElsayed.n@gmail.com>
2020-06-16 23:55:41 +0200

    add product component

---
+++ src/app/products/proudectList.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './proudectList.component.html'
+  })
+export class ProductLstComponent{
+// test 
+}
